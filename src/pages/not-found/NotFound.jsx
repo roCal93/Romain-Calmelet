25af94fc8762de85a59feb4ccebf79daee306e4d
Romain Calmelet <romaincalmelet@gmail.com>
2025-06-05 13:50:39 +0200

    deploy

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -4,7 +4,7 @@ import styles from './notFound.module.scss'
 const NotFound = () => {
   return (
     <div>
-      <title>Argent Bank - Page Introuvable</title>
+      <title>Romain Calmelet - Page Introuvable</title>
       <div className={styles.notFound}>
         <h2 className={styles.title}>404</h2>
         <h2>Oups! La page que vous demandez n'existe pas.</h2>
